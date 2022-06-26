fee926ae84b4ccbd6e50783a52275a7d58accc43
serranojl <serranojohnlloyd78@gmail.com>
2022-06-26 10:38:48 +0800

    add animation movement script to player

---
+++ scripts/Player.gd
@@ -0,0 +1,51 @@
+extends KinematicBody2D
+
+var velocity = Vector2.ZERO
+
+const ACCELERATION = 500
+const MAX_SPEED = 80
+const FRICTION = 500
+
+enum {
+	MOVE,
+	ROLL,
+	ATTACK
+}
+
+var state = MOVE
+
+onready var animationPlayer = $AnimationPlayer
+onready var animationTree = $AnimationTree
+onready var animationState = animationTree.get("parameters/playback")
+
+func _ready():
+	animationTree.active = true
+
+func _physics_process(delta):
+	match state:
+		MOVE:
+			move_state(delta)
+		ROLL:
+			pass
+		ATTACK:
+			pass
+	
+func move_state(delta):
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	input_vector.y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
+	input_vector = input_vector.normalized()
+	
+	if input_vector != Vector2.ZERO:
+		animationTree.set("parameters/Idle/blend_position", input_vector)
+		animationTree.set("parameters/Run/blend_position", input_vector)
+		animationState.travel("Run")
+		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
+	else:
+		animationState.travel("Idle")
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+	
+	move()
+	
+func move():
+	velocity = move_and_slide(velocity)
