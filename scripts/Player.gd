d5d72f026081143d7fd9b5e00335d813d7a548d8
serranojl <serranojohnlloyd78@gmail.com>
2022-06-29 21:39:11 +0800

    add player attack script

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -28,7 +28,7 @@ func _physics_process(delta):
 		ROLL:
 			pass
 		ATTACK:
-			pass
+			attack_state(delta)
 	
 func move_state(delta):
 	var input_vector = Vector2.ZERO
@@ -39,6 +39,7 @@ func move_state(delta):
 	if input_vector != Vector2.ZERO:
 		animationTree.set("parameters/Idle/blend_position", input_vector)
 		animationTree.set("parameters/Run/blend_position", input_vector)
+		animationTree.set("parameters/Attack/blend_position", input_vector)
 		animationState.travel("Run")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
@@ -47,5 +48,15 @@ func move_state(delta):
 	
 	move()
 	
+	if Input.is_action_just_pressed("attack"):
+		state = ATTACK
+	
 func move():
 	velocity = move_and_slide(velocity)
+	
+func attack_state(delta):
+	velocity = Vector2.ZERO
+	animationState.travel("Attack")
+
+func attack_animation_finished():
+	state = MOVE
